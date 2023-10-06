136f571bb6f4ca7a83cb5518dc45fcb86707aeb8
Reyber Akpinar <rakpinar@thinkport.digital>
2023-10-06 21:05:52 +0200

    Schema korregiert sowie roleBinding

--- roleBinding.tf
+++ roleBinding.tf
@@ -102,3 +102,37 @@ resource "confluent_role_binding" "app-consumer-developer-read-from-group" {
   role_name   = "DeveloperRead"
   crn_pattern = "${confluent_kafka_cluster.standard.rbac_crn}/kafka=${confluent_kafka_cluster.standard.id}/group=confluent_cli_consumer_*"
 }
+
+resource "confluent_service_account" "env-manager" {
+  display_name = "env-manager"
+  description  = "Service account to manage 'Staging' environment"
+}
+
+resource "confluent_role_binding" "env-manager-data-steward" {
+  principal   = "User:${confluent_service_account.env-manager.id}"
+  role_name   = "DataSteward"
+  crn_pattern = confluent_environment.staging.resource_name
+}
+
+resource "confluent_api_key" "env-manager-schema-registry-api-key" {
+  display_name = "env-manager-schema-registry-api-key"
+  description  = "Schema Registry API Key that is owned by 'env-manager' service account"
+  owner {
+    id          = confluent_service_account.env-manager.id
+    api_version = confluent_service_account.env-manager.api_version
+    kind        = confluent_service_account.env-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_schema_registry_cluster.essentials.id
+    api_version = confluent_schema_registry_cluster.essentials.api_version
+    kind        = confluent_schema_registry_cluster.essentials.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+  depends_on = [
+    confluent_role_binding.env-manager-data-steward
+  ]
+}
--- schema.tf
+++ schema.tf
@@ -15,3 +15,18 @@ resource "confluent_schema_registry_cluster" "essentials" {
     id = data.confluent_schema_registry_region.essentials.id
   }
 }
+
+resource "confluent_schema" "orders" {
+  schema_registry_cluster {
+    id = confluent_schema_registry_cluster.essentials.id
+  }
+  rest_endpoint = confluent_schema_registry_cluster.essentials.rest_endpoint
+  # https://developer.confluent.io/learn-kafka/schema-registry/schema-subjects/#topicnamestrategy
+  subject_name = "${confluent_kafka_topic.orders.topic_name}-value"
+  format       = "AVRO"
+  schema       = file("./schemas/avro/orders.avsc")
+  credentials {
+    key    = confluent_api_key.env-manager-schema-registry-api-key.id
+    secret = confluent_api_key.env-manager-schema-registry-api-key.secret
+  }
+}
